db2e4ba18ec30d81da5fcb04d109c49805b8c0b4
kevin <kevingao52997@gmail.com>
2022-06-23 10:46:17 -0700

    created api routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-// const csrf = require('csurf');
 const router = express.Router();
-// const csrfProtection = csrf({ cookie: true });
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
