89f11fc40781b5ba8c1e30be9fe0190d0885f03c
kevin <kevingao52997@gmail.com>
2022-06-27 10:26:13 -0700

    added demo user functionality

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
+import DemoUser from '../DemoUser';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -17,6 +18,7 @@ function Navigation({ isLoaded }){
     sessionLinks = (
       <>
         <LoginFormModal />
+        <DemoUser />
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
