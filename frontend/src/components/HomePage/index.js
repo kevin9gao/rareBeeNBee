6b81eafc939019e99fbe285285aa0d39cf0aeddf
kevin <kevingao52997@gmail.com>
2022-06-27 21:44:41 -0700

    css tweaks, working on clickable homepage links

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Route } from "react-router-dom";
 import { getBees } from '../../store/bees';
+import SingleBee from "../SingleBee";
 import './HomePage.css';
 
 function HomePage () {
@@ -19,9 +21,11 @@ function HomePage () {
         {bees.map(bee => {
           return (
             <article className="bee-cards">
-              <a>
-                <img src={bee.imageUrl} alt={bee.name} className='bee-pics' />
-              </a>
+              <img src={bee.imageUrl} alt={bee.name} className='bee-pics' />
+                {/* blocker */}
+                <NavLink to={`/api/bees/${bee.id}`}>
+                  <SingleBee bee={bee.id} />
+                </NavLink>
               <div className="card-content">
                 <p className="card-location">
                   {`${bee.city}, ${bee.state}, ${bee.country}`}
