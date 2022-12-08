b0bd2f80ef47f6ca5c7924e7af900345d6a10bf5
kevin <kevingao52997@gmail.com>
2022-12-07 17:47:52 -0800

    added locale bar no functionality yet

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getBees } from '../../store/bees';
 import './HomePage.css';
+import LocaleBar from "./LocaleBar";
 
 function HomePage() {
   const dispatch = useDispatch();
+  const [localeFilter, setLocaleFilter] = useState('');
+  console.log('localeFilter', localeFilter);
 
   const bees = useSelector(state => {
     return state.bees.list.map(beeId => state.bees[beeId]);
@@ -19,25 +22,30 @@ function HomePage() {
     return (
       <main>
         <div className="main-wrapper">
-          {bees.map(bee => {
-            return (bee && (
-              <NavLink
-                className='card-navlinks'
-                key={bee.id}
-                exact to={`/bees/${bee.id}`}
-              >
-                <article className="bee-cards">
-                  <img src={bee.imageUrl} alt={bee.name} className='bee-pics' />
-                  <div className="card-content">
-                    <h4 className="bee-names">{bee.name}</h4>
-                    <p className="card-location">
-                      {`${bee.city}, ${bee.state}, ${bee.country}`}
-                    </p>
-                  </div>
-                </article>
-              </NavLink>
-            ));
-          })}
+          <div className="hp-locale-wrapper">
+            <LocaleBar setLocaleFilter={setLocaleFilter} />
+          </div>
+          <div id="bee-grid">
+            {bees.map(bee => {
+              return (bee && (
+                <NavLink
+                  className='card-navlinks'
+                  key={bee.id}
+                  exact to={`/bees/${bee.id}`}
+                >
+                  <article className="bee-cards">
+                    <img src={bee.imageUrl} alt={bee.name} className='bee-pics' />
+                    <div className="card-content">
+                      <h4 className="bee-names">{bee.name}</h4>
+                      <p className="card-location">
+                        {`${bee.city}, ${bee.state}, ${bee.country}`}
+                      </p>
+                    </div>
+                  </article>
+                </NavLink>
+              ));
+            })}
+          </div>
         </div>
       </main>
     );
